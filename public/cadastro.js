72a5752004ddd1f1e36469501a2c78a2c0adc371
AraujoVE <dearaujoviniciuse@gmail.com>
2020-11-12 21:46:09 -0300

    Agora esta separadinho

--- public/cadastro.js
+++ public/cadastro.js
@@ -1,14 +1,14 @@
-const signInForm = document.getElementById("sign_up_form");
+const passwordError = document.getElementById("pwd-error-msg-holder");
+const signUpForm = document.getElementById("sign_up_form");
 const cadastroButton = document.getElementById("su_submit");
 const passwordConfirm = document.getElementById("su_pwd2");
-const passwordError = document.getElementById("pwd-error-msg-holder");
 
 function comparePwds(){
-    alert("oi");
-    const password1 = signInForm.su_pwd1.value;
-    const password2 = signInForm.su_pwd2.value;
+    const password1 = signUpForm.su_pwd1.value;
+    const password2 = signUpForm.su_pwd2.value;
+
     if(password1 === password2) passwordError.style.display = "none";
-    else passwordError.style.display = "block" 
+    else passwordError.style.display = "block"; 
 }
 
 cadastroButton.addEventListener("click", (e) => {
