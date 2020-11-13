bf12d0d45a0c9b83f62c4e027f7cbd1f3802a36c
AraujoVE <dearaujoviniciuse@gmail.com>
2020-11-12 21:32:43 -0300

    Quase finalizado js

--- public/cadastro.js
+++ public/cadastro.js
@@ -1,8 +1,17 @@
-const signInForm = document.getElementById("sign_in_form");
-const loginButton = document.getElementById("si_submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+const signInForm = document.getElementById("sign_up_form");
+const cadastroButton = document.getElementById("su_submit");
+const passwordConfirm = document.getElementById("su_pwd2");
+const passwordError = document.getElementById("pwd-error-msg-holder");
 
-loginButton.addEventListener("click", (e) => {
+function comparePwds(){
+    alert("oi");
+    const password1 = signInForm.su_pwd1.value;
+    const password2 = signInForm.su_pwd2.value;
+    if(password1 === password2) passwordError.style.display = "none";
+    else passwordError.style.display = "block" 
+}
+
+cadastroButton.addEventListener("click", (e) => {
     e.preventDefault();
     const email = signInForm.si_email.value;
     const password = signInForm.si_pwd.value;
