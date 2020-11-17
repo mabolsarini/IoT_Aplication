288dba4fce66eb24e72259b388c23a75e0c332fe
AraujoVE <dearaujoviniciuse@gmail.com>
2020-11-17 20:06:28 -0300

    iterativo

--- public/js/app.js
+++ public/js/app.js
@@ -3,4 +3,18 @@ function switchPower() {
         "/power",
         {}
     );
+    ioAc.value == "Ligar" ? ioAc.value = "Desligar" : ioAc.value = "Ligar";
+    return true;
+}
+
+function changeRangeVal(id){
+    const curr = document.getElementById(id);
+    const currVal = document.getElementById(id+"Val");
+    currVal.innerHTML = curr.value;
+}
+
+function iniValues(){
+    tMinVal.innerHTML = 19;
+    tMaxVal.innerHTML = 20;
+    delayVal.innerHTML = 61;
 }
