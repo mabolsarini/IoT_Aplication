37668e25494d19a044262bf611618336ca15f2ae
AraujoVE <dearaujoviniciuse@gmail.com>
2020-11-18 16:20:08 -0300

    sensores de qtd variavel adicionados

--- public/js/app.js
+++ public/js/app.js
@@ -3,32 +3,39 @@ const tMax = document.getElementById("tMax");
 const tOp = document.getElementById("tOp");
 const delay = document.getElementById("delay");
 const cardConfig = document.getElementById("cardConfig");
+var toSubmit = false;
 
+var sensorsQtty = {
+    Temp : 6,
+    Umid : 1,
+    Lumi : 2,
+    Move : 2
+}
 
 var sensors = {
-    Temp : [1.1,
-            1.2,
-            1.3,
-            1.4,
-            1.5,
-            1.6],
-
-    Umid : [2.1],
-
-    Lumi:  [3.1,
-            3.2],
-
-    Move:  [4.1,
-            4.2]
+    Temp: [0],
+    Umid: [0],
+    Lumi: [0],
+    Move: [0],
 }
 
 
 function submitConfig(){
-    if(tMin.value > tMax.value){
-        alert("Erro");
-        return false;
-    }
-    return true;
+    toSubmit = false;
+    if(document.getElementById("tMinVal").innerHTML != tMin.value) alert("Erro: Temperatura mínima real diferente da temperatua mínima aparente.");
+    else if(tMin.value < 16) alert("Erro: Temperatura mínima menor que o mínimo permitido.");
+    else if(tMin.value > 22) alert("Erro: Temperatura mínima maior que o máximo permitido.");
+    else if(document.getElementById("tMaxVal").innerHTML != tMax.value) alert("Erro: Temperatura maxima real diferente da temperatua maxima aparente.");
+    else if(tMax.value < 17) alert("Erro: Temperatura máxima menor que o mínimo permitido.");
+    else if(tMax.value > 23) alert("Erro: Temperatura máxima maior que o máximo permitido.");
+    else if(document.getElementById("tOpVal").innerHTML != tOp.value) alert("Erro: Valor inválido.");
+    else if(tMin.value > tMax.value) alert("Erro: Temperatura mínima maior do que a temperatura máxima.");
+    else if(tOp.value>tMax.value) alert("Erro: Temperatura de operação maior do que a temperatura máxima.")
+    else if(tOp.value<tMin.value) alert("Erro: Temperatura de operação menor do que a temperatura mínima.")
+    else if(document.getElementById("delayVal").innerHTML != delay.value) alert("Erro: Delay real diferente do delay aparente.");
+    else if(delay.value < 1) alert("Erro: Delay menor que o mínimo permitido.");
+    else if(delay.value > 120) alert("Erro: Delay maior que o máximo permitido.");
+    else toSubmit = true;
 }
 
 function switchPower() {
@@ -84,6 +91,16 @@ function setTempMax(){
     tOp.max = tMax.value;
 }
 
+function addSensors(name,pos,mocar) {
+    var element = document.createElement("LI");
+    var textnode = document.createTextNode(pos+" : ");
+    element.appendChild(textnode);
+    element.id = name +"_"+pos;
+    element.className = "list-subgroup-item";
+    document.getElementById(name+"_List").appendChild(element);
+    if(mocar) document.getElementById(name +"_"+pos).innerHTML += '<span style="color: blue;">'+name[0]+'.'+pos+'</span>';
+
+}
 
 function iniValues(){
     tMin.value = 16;
@@ -98,9 +115,9 @@ function iniValues(){
     delay.value = 10;
     changeRangeVal("delay");
 
-    for(i=1;i<=6;i++) document.getElementById("Temp_"+i).innerHTML += '<span style="color: blue;">'+ sensors.Temp[i-1] + '</span>';
-    for(i=1;i<=1;i++) document.getElementById("Umid_"+i).innerHTML += '<span style="color: blue;">'+ sensors.Umid[i-1] + '</span>';    
-    for(i=1;i<=2;i++) document.getElementById("Lumi_"+i).innerHTML += '<span style="color: blue;">'+ sensors.Lumi[i-1] + '</span>';
-    for(i=1;i<=2;i++) document.getElementById("Move_"+i).innerHTML += '<span style="color: blue;">'+ sensors.Move[i-1] + '</span>';
+    for(i=1;i<=sensorsQtty.Temp;i++) addSensors("Temp",i,true);
+    for(i=1;i<=sensorsQtty.Umid;i++) addSensors("Umid",i,true);
+    for(i=1;i<=sensorsQtty.Lumi;i++) addSensors("Lumi",i,true);
+    for(i=1;i<=sensorsQtty.Move;i++) addSensors("Move",i,true);
 
 }
